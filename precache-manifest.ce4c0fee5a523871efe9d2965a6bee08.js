2981dc2cd5a0f94e7e17e552d6cfbf5cdcba3370
kanraghav <kan.raghav@capco.com>
2019-04-11 13:13:40 +0530

    Updates

---
+++ precache-manifest.ce4c0fee5a523871efe9d2965a6bee08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactJS/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9549956a261461a708b4",
+    "url": "/ReactJS/static/js/runtime~main.9549956a.js"
+  },
+  {
+    "revision": "5aad127c9c389566ca6c",
+    "url": "/ReactJS/static/js/main.5aad127c.chunk.js"
+  },
+  {
+    "revision": "86b76d7d0cb0298adf1f",
+    "url": "/ReactJS/static/js/1.86b76d7d.chunk.js"
+  },
+  {
+    "revision": "5aad127c9c389566ca6c",
+    "url": "/ReactJS/static/css/main.cbc3e5d2.chunk.css"
+  },
+  {
+    "revision": "86b76d7d0cb0298adf1f",
+    "url": "/ReactJS/static/css/1.6fe1ef11.chunk.css"
+  },
+  {
+    "revision": "8c2385fb4cafd8493806a23b841bf745",
+    "url": "/ReactJS/index.html"
+  }
+];
